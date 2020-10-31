b19b03b86ac12bb8825b7ae837047afe5833cbad
Your Name <andangsaefullah4@gmail.com>
2020-10-31 14:46:25 +0700

    Add project files.

---
+++ Client_AndangSaefullah_032/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P2_032;
+
+namespace Client_AndangSaefullah_032
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            double hasilKurang = obj.Kurang(3, 2);
+            double hasilKali = obj.Kali(2, 2);
+            double hasilBagi = obj.Bagi(2, 2);
+
+        }
+    }
+}
